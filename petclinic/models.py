e2303fbeb8e0fcec69684c5d9256c65c985d6904
Thomas Wöhlke <thomas.woehlke@gmail.com>
2019-10-29 20:21:28 +0100

    model

--- petclinic/models.py
+++ petclinic/models.py
@@ -9,40 +9,40 @@ class Specialty(models.Model):
 
 
 class Veterinarian(models.Model):
-    veterinarian_firstName = models.CharField(max_length=200)
-    veterinarian_lastName = models.CharField(max_length=200)
-    specialties = models.ManyToManyField(Specialty)
+    veterinarian_first_name = models.CharField(max_length=200)
+    veterinarian_last_name = models.CharField(max_length=200)
+    veterinarian_specialties = models.ManyToManyField(Specialty)
 
     def __str__(self):
-        return self.veterinarian_firstName + self.veterinarian_lastName
+        return self.veterinarian_first_name + self.veterinarian_last_name
 
 
 class Owner(models.Model):
-    owner_firstName = models.CharField(max_length=200)
-    owner_lastName = models.CharField(max_length=200)
+    owner_first_name = models.CharField(max_length=200)
+    owner_last_name = models.CharField(max_length=200)
     owner_address = models.CharField(max_length=200)
-    owner_houseNumber = models.CharField(max_length=200)
-    owner_addressInfo = models.CharField(max_length=200)
+    owner_house_number = models.CharField(max_length=200)
+    owner_address_info = models.CharField(max_length=200)
     owner_city = models.CharField(max_length=200)
-    owner_zipCode = models.CharField(max_length=200)
-    owner_phoneNumber = models.CharField(max_length=200)
+    owner_zip_code = models.CharField(max_length=200)
+    owner_phone_number = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
-        return self.owner_firstName + self.owner_lastName
+        return self.owner_first_name + self.owner_last_name
 
 
 class PetType(models.Model):
-    petType_text = models.CharField(max_length=200, unique=True)
+    pet_type_text = models.CharField(max_length=200, unique=True)
 
     def __str__(self):
-        return self.petType_text
+        return self.pet_type_text
 
 
 class Pet(models.Model):
     pet_name = models.CharField(max_length=200)
-    pet_dateOfBirth = models.DateField()
-    petType = models.ForeignKey(PetType, on_delete=models.CASCADE)
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    pet_date_of_birth = models.DateField()
+    pet_type = models.ForeignKey(PetType, on_delete=models.CASCADE)
+    pet_owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.pet_name
@@ -51,7 +51,7 @@ class Pet(models.Model):
 class Visit(models.Model):
     visit_text = models.CharField(max_length=200)
     visit_date = models.DateField()
-    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    visit_pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.visit_text
