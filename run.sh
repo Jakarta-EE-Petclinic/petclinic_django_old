a49335bc57127d66ad77e57c36bb62a57b793d82
thomaswoehlke <thomas.woehlke@gmail.com>
2019-10-31 23:24:34 +0100

    setup linux

--- run.sh
+++ run.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
+
+#sudo apt-get install python3-pip
+#sudo pip3 install virtualenv
+#virtualenv venv
+#source venv/bin/activate
+
+
+
+
 #source venv/bin/activate
 python manage.py runserver
 #deactivate
