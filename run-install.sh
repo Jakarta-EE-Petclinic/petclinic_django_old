b63d493ece7d6a1c10daa1084a462a42757710ee
Thomas Wöhlke <thomas.woehlke@gmail.com>
2019-11-01 07:55:02 +0100

    views

---
+++ run-install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+sudo apt-get install python3-pip
+sudo pip3 install virtualenv
+virtualenv venv
+source venv/bin/activate
+
+
+# https://github.com/jieter/django-tables2
+# https://django-tables2.readthedocs.io/en/latest/pages/installation.html
+pip install django-tables2
+pip install django-debug-toolbar
+pip install django-extensions
+pip install django-allauth
+
+pip install django-crispy-forms
+pip install django-generic-scaffold
+#pip install django-compressor
+pip install django-crispy-forms
+pip install django-generic-scaffold
+pip install django-import-export
--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env zsh
 
 
-#sudo apt-get install python3-pip
-#sudo pip3 install virtualenv
-#virtualenv venv
-#source venv/bin/activate
+
 
 
 
