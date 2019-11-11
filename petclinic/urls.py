dca321acae31d1b718b0b30e9f53dfaf3d28f4b8
Thomas Woehlke <thomas.woehlke@gmail.com>
2019-11-11 01:04:09 +0100

    next dev step

--- petclinic/urls.py
+++ petclinic/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
 
 from . import views
+from django.views.decorators.cache import never_cache
 
 app_name = 'petclinic'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('specialty/', views.SpecialtyList.as_view(), name='specialty'),
-    path('specialty/<int:id>/', views.SpecialtyDetail.as_view(), name='specialty_detail'),
-    path('pettype/', views.PetTypeList.as_view(), name='pettype'),
-    path('pettype/<int:id>/', views.PetTypeDetail.as_view(), name='pettype_detail'),
-    path('veterinarian/', views.VeterinarianList.as_view(), name='veterinarian'),
-    path('veterinarian/<int:id>/', views.VeterinarianDetail.as_view(), name='veterinarian_detail'),
-    path('visit/', views.VisitList.as_view(), name='visit'),
-    path('visit/<int:id>', views.VisitDetail.as_view(), name='visit_detail'),
-    path('pet/', views.PetList.as_view(), name='pet'),
-    path('pet/<int:id>', views.PetDetail.as_view(), name='pet_detail'),
-    path('owner/', views.OwnerList.as_view(), name='owner'),
-    path('owner/<int:id>', views.OwnerDetail.as_view(), name='owner_detail'),
+    path('specialty/', never_cache(views.SpecialtyList.as_view()), name='specialty'),
+    path('specialty/<int:id>/', never_cache(views.SpecialtyDetail.as_view()), name='specialty_detail'),
+    path('pettype/', never_cache(views.PetTypeList.as_view()), name='pettype'),
+    path('pettype/<int:id>/', never_cache(views.PetTypeDetail.as_view()), name='pettype_detail'),
+    path('veterinarian/', never_cache(views.VeterinarianList.as_view()), name='veterinarian'),
+    path('veterinarian/<int:id>/', never_cache(views.VeterinarianDetail.as_view()), name='veterinarian_detail'),
+    path('visit/', never_cache(views.VisitList.as_view()), name='visit'),
+    path('visit/<int:id>', never_cache(views.VisitDetail.as_view()), name='visit_detail'),
+    path('pet/', never_cache(views.PetList.as_view()), name='pet'),
+    path('pet/<int:id>', never_cache(views.PetDetail.as_view()), name='pet_detail'),
+    path('owner/', never_cache(views.OwnerList.as_view()), name='owner'),
+    path('owner/<int:id>', never_cache(views.OwnerDetail.as_view()), name='owner_detail'),
 ]
